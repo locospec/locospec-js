2c2000aa10b1178d214905482e5eb6adf478cbb8
Rajiv Seelam <rajiv@betalectic.com>
2022-10-13 11:21:32 +0530

    Expore executeActionInternally

--- packages/engine/index.js
+++ packages/engine/index.js
@@ -1,5 +1,6 @@
 const locoFactory = require("./factory");
 const executeAction = require("./actions");
 const executeRoute = require("./executeRoute");
+const executeActionInternally = require("./executeActionInternally");
 
-module.exports = { executeRoute, locoFactory };
+module.exports = { executeRoute, locoFactory, executeActionInternally };
