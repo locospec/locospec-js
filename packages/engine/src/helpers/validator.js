89acb0b3eae2eeaeb45d7b45475d114e817815f9
Betalectic <betalectic@Betalectics-MacBook-Air.local>
2026-05-20 04:05:13 -0700

    feat: remove validate.js dependency from package.json and add local validate.js file

--- packages/engine/src/helpers/validator.js
+++ packages/engine/src/helpers/validator.js
@@ -1,4 +1,4 @@
-var validate = require("validate.js");
+var validate = require("../validate.js");
 const runOperations = require("./runOperations");
 
 validate.validators.outside_function = function (
